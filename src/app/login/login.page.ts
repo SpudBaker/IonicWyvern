8b865efdcdd1b6de3634c8b5ffceb1e28a5cc2ef
Richard Baker <richard.baker@expensys.com>
2021-11-17 19:06:58 +0000

    reset password

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy } from '@angular/core';
-import { Auth, signInWithEmailAndPassword, signOut, user, User } from '@angular/fire/auth';
+import { Auth, sendPasswordResetEmail,signInWithEmailAndPassword, signOut, user, User } from '@angular/fire/auth';
+import { FirebaseError } from '@firebase/util';
 import { Observable, Subscription } from 'rxjs';
 
 @Component({
@@ -9,8 +10,9 @@ import { Observable, Subscription } from 'rxjs';
 })
 export class LoginPage implements OnDestroy {
 
-  private user$: Observable<User | null>;
+  public user$: Observable<User | null>;
   private userSubscription: Subscription;
+  public loginErrMessage: string;
   public inputEmail: string;
   public inputPassword: string;
   public initialised = false;
@@ -27,15 +29,36 @@ export class LoginPage implements OnDestroy {
     this.userSubscription.unsubscribe();
   }
 
-  async login(){
-    signInWithEmailAndPassword(this.auth, this.inputEmail, this.inputPassword);
+  login(){
+    this.loginErrMessage = null;
+    signInWithEmailAndPassword(this.auth, this.inputEmail, this.inputPassword).catch(e => {
+      let err = e as FirebaseError;
+      switch (err.code){
+        case("auth/user-not-found"):
+          this.loginErrMessage = 'email not recognised, have you registered?';
+          return;
+        case ("auth/wrong-password"):
+          this.loginErrMessage = 'wrong password - please try again or reset';
+          return;
+        case ("auth/too-many-requests"):
+          this.loginErrMessage = 'too many attempts and account is locked. Please reset password.';
+          return;
+      }
+    });
+  }
+
+  async resetPassword(){
+    await sendPasswordResetEmail(this.auth, this.inputEmail);
+    this.loginErrMessage = 'Please check your inbox for an email with a password reset link';
+
   }
 
   logOut(){
     signOut(this.auth);
   }
 
-  newUser(){
+  register(){
+
   }
 
 }
